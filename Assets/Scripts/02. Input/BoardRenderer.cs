305292233abbdcdc699fb52fc47f241045706ec9
ahn eun seong <tjddmsdks@gmail.com>
2026-02-09 18:22:48 +0900

    프로젝트 초기화

---
+++ Assets/Scripts/02. Input/BoardRenderer.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class BoardRenderer : MonoBehaviour
+{
+    public enum StoneColor
+    {
+        Black,
+        White,
+    }
+
+    public void PlaceStoneObj(float x, float y, StoneColor color)
+    {
+    }
+
+    public void ShowLastStoneMarker()
+    {
+    }
+}
