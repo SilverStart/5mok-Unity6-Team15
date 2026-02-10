3bf44f475911dec750240baf8e79a3e9a1a02027
ahn eun seong <tjddmsdks@gmail.com>
2026-02-10 16:56:44 +0900

    마우스 입력을 감지하여 화면에 오목알을 두는 기능 추가

--- Assets/Scripts/02. Input/BoardRenderer.cs
+++ Assets/Scripts/02. Input/BoardRenderer.cs
@@ -2,17 +2,46 @@ using UnityEngine;
 
 public class BoardRenderer : MonoBehaviour
 {
-    public enum StoneColor
+    [SerializeField] private GameObject blackStonePrefab;
+    [SerializeField] private GameObject whiteStonePrefab;
+
+    [SerializeField] private Transform startPoint;
+
+    [SerializeField] private float cellSize = 0.4f;
+
+    public void PlaceStoneObj(int x, int y, StoneColor color)
     {
-        Black,
-        White,
+        Vector3 spawnPosition = GetWorldPosition(x, y);
+
+        GameObject stonePrefab = color == StoneColor.White ? whiteStonePrefab : blackStonePrefab;
+        Instantiate(stonePrefab, spawnPosition, Quaternion.identity, transform);
     }
 
-    public void PlaceStoneObj(float x, float y, StoneColor color)
+    private Vector3 GetWorldPosition(int x, int y)
     {
+        float worldX = startPoint.position.x + (cellSize * x);
+        float worldY = startPoint.position.y - (cellSize * y);
+
+        return new Vector3(worldX, worldY, 0);
     }
 
     public void ShowLastStoneMarker()
     {
     }
+
+    public Transform GetStartPoint()
+    {
+        return startPoint;
+    }
+
+    public float GetCellSize()
+    {
+        return cellSize;
+    }
+}
+
+public enum StoneColor
+{
+    Black,
+    White,
 }
