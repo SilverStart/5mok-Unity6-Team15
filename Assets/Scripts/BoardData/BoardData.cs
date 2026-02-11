b41c13a80150aedf09ac2e107403d2ea5378a50b
Jokuhus <tjrwngh2@gmail.com>
2026-02-11 14:45:52 +0900

    edit: BoardData 에 주석 추가

--- Assets/Scripts/BoardData/BoardData.cs
+++ Assets/Scripts/BoardData/BoardData.cs
@@ -2,22 +2,30 @@ using common;
 
 public class BoardData
 {
+    // 보드의 가로와 세로 길이
     private int _width, _height;
 
+    // 보드의 상태
     private Constants.StoneColor[,] _board;
+
+    // 보드 정보 (복사)
     public Constants.StoneColor[,] Board { get { return (Constants.StoneColor[,])_board.Clone();}}
 
     public BoardData(int width, int height)
     {
+        // 보드의 가로와 세로 길이 정보 저장
         _width = width;
         _height = height;
 
+        // 보드 정보 초기화
         _board = new Constants.StoneColor[_width, _height];
         Clear();
     }
 
+    // 보드에 착수
     public void SetStone(int x, int y, Constants.StoneColor color)
     {
+        // 착수 가능 범위 검사
         if (x >= 0 && x < _width &&
             y >= 0 && y < _height &&
             _board[x, y] == Constants.StoneColor.None)
@@ -26,6 +34,7 @@ public class BoardData
         }
     }
 
+    // 보드 정보 초기화
     public void Clear()
     {
         for (int row = 0; row < _width; row++)
