cadaeab83d8f3fc4f6798cc03b177a8db8253930
ahn eun seong <tjddmsdks@gmail.com>
2026-02-11 16:20:42 +0900

    PlayerPrefs 를 Unity Editor 에서 지우는 기능 추가

---
+++ Assets/Scripts/Editor/PlayerPrefsCleaner.cs
@@ -0,0 +1,17 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+
+namespace Editor
+{
+    public class PlayerPrefsCleaner
+    {
+        [MenuItem("Tools/Clear PlayerPrefs")]
+        public static void ClearPlayerPrefs()
+        {
+            PlayerPrefs.DeleteAll();
+            Debug.Log("PlayerPrefs 데이터 삭제 완료");
+        }
+    }
+}
+#endif
