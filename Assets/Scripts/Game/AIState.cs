26b8829d4e8a7d164872d5e896d0441bffa3c2d8
Jokuhus <tjrwngh2@gmail.com>
2026-02-26 17:12:53 +0900

    add: GameLogic 과 각각 플레이어, ai state 추가

---
+++ Assets/Scripts/Game/AIState.cs
@@ -0,0 +1,48 @@
+using System.Threading.Tasks;
+using UnityEngine;
+using static common.Constants;
+
+public class AIState : BaseState
+{
+    public AIState(StoneColor color, GameLogic gameLogic)
+    {
+        _color = color;
+        _gameLogic = gameLogic;
+    }
+
+    // 턴 변경
+    public override void HandleNextTurn()
+    {
+        _gameLogic.ChangeGameState();
+    }
+
+    public override async Task OnEnter()
+    {
+        // OX UI 업데이트
+        GameManager.Instance.SetGameTurn(_color);
+
+        // 상태 진입 시 로직 구현
+        while (!isValidMove)
+        {
+            if (await _gameLogic.GetAIMove(_color) is (int x, int y) targetMove)
+            {
+                // 블록이 클릭되었을 때 처리할 로직
+                HandleMove(x, y);
+            }
+            else
+            {
+                _gameLogic.Resigne(_color);
+                break;
+            }
+        }
+    }
+
+    public override void HandleMove(int x, int y)
+    {
+        ProcessMove(x, y);
+    }
+
+    public override void OnExit()
+    {
+    }
+}
